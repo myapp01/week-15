ecfea80d06b1aef6447673ad33ab41e810927a16
myapp01 <69285962+myapp01@users.noreply.github.com>
2021-02-23 11:53:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+// let year = 1972;
+let year = prompt("Enter value of Year:");
+if (year == 1972 || year == 1984 || year == 1996 || year == 2008 || year ==2020){
+    alert("Rat ZODIAC SINGS");
+    console.log("Rat ZODIAC SINGS");
+    document.getElementById('hContent').innerHTML = 'Rat ZODIAC SINGS' ;
+}
+else if(year == 1973 || year == 1985 || year == 1997 || year == 2009|| year ==2021){
+    alert("Oxen ZODIAC SINGS");
+    console.log("Oxen ZODIAC SINGS");
+    document.getElementById('hContent').innerHTML = 'Oxen ZODIAC SINGS' ;
+}
+else if(year == 1974 || year == 1986 || year == 1998 || year == 2010|| year ==2022){
+    alert("Tiger ZODIAC SINGS");
+    console.log("Tiger ZODIAC SINGS");
+    document.getElementById('hContent').innerHTML = 'Tiger ZODIAC SINGS' ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+else if(year == 1973 || year == 1985 || year == 1997 || year == 2009|| year ==2021){
+    alert("Error ZODIAC SINGS");
+    console.log("Error ZODIAC SINGS");
+    document.getElementById('hContent').innerHTML = 'Error ZODIAC SINGS' ;
+}
